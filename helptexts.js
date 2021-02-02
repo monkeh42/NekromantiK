eda7418dd8d9f1224931e39a1980d306af8ce528
monkeh42 <72186713+monkeh42@users.noreply.github.com>
2021-02-01 19:05:50 -0600

    Update helptexts.js

--- helptexts.js
+++ helptexts.js
@@ -6,7 +6,8 @@ const HELP_TEXTS = {
                     Each unit also has a corpse multiplier that multiplies your total corpse gain (additively), and a unit multiplier, which multiplies the unit production of that tier and the tier below it. (additively). \
                     Every time you purchase one of a unit after the first, its cost and corpse multiplier are multiplied by constants. The base unit multiplier is always equal to the square root of the corpse multiplier.<br>\
                     Cost multipliers by unit tier (first tier = zombies): 100, 1e4, 1e4, 1e6, 1e10, 1e11, 1e12, 1e15.<br>\
-                    Multiplier multipliers by unit tier: 1.75, 2, 2, 2, 2.2, 2.2, 2.5, 2.5.',
+                    Multiplier multipliers by unit tier: 1.75, 2, 2, 2, 2.2, 2.2, 2.5, 2.5.<br>\
+                    (more sections will appear here as you progress)',
         'spacePrestige': '<h2>NEW WORLDS</h2>\
                         Eventually, this world will run out of souls to torture and corpses to harvest. When this happens, you must \'space prestige\' to move on to a new world.<br>\
                         When you space prestige, you lose all of your corpses and units, and you gain one Conquered World. Your conquered worlds provide a new multiplier to total corpse gain. Your first three space prestiges \
