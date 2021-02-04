07d5e4585990cf80d27544891ce33c55371c0c4d
monkeh42 <72186713+monkeh42@users.noreply.github.com>
2021-02-04 01:21:53 -0600

    Update units.js

--- units.js
+++ units.js
@@ -12,9 +12,9 @@ const UNITS_DATA = {
         },
         corpseMult: function() {
             var m = this.baseMultPer;
-            if (hasUpgrade(1, 12)) { m = m.times(getUpgEffect(1, 12)); }
             if (player.units[this.tier].bought.eq(0)) { return new Decimal(0); }
             m = m.pow(player.units[this.tier].bought-1);
+            if (hasUpgrade(1, 11)) m = m.times(getUpgEffect(1, 11));
             return m;
         },
         prodMult: function() {
